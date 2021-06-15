82550e78bd6494fca6082cd4b16c5728c0be9241
estephanus <stephan.waitz@googlemail.com>
2021-06-15 11:47:05 +0200

    prepare files for use with ditto

--- code/ditto_helper.py
+++ code/ditto_helper.py
@@ -39,3 +39,20 @@ def to_jsonl(input_file, output_file):
       row_list = [left_dict,right_dict]
       json.dump(row_list, outfile)
       outfile.write('\n')
+
+def prepare_file(input_file, output_file):
+  df_input = pd.read_csv(input_file).drop('id',axis=1)
+  file = open(output_file,"a")
+  left_atts = [col for col in df_input.columns if col.startswith('left')]
+  right_atts = [col for col in df_input.columns if col.startswith('right')]
+  df_dict = df_input.to_dict(orient='records')
+  for row in df_dict:
+    content = ''
+    for attr in left_atts:
+      content += 'COL %s VAL %s ' % (removeprefix(attr,'left_'), re.sub('\t', ' ', str(row[attr])))
+    content += ' \t '
+    for attr in right_atts:
+      content += 'COL %s VAL %s ' % (removeprefix(attr,'right_'), re.sub('\t', ' ', str(row[attr])))
+    content += ' \t%s\n' % (row['label'])
+    file.write(content)
+  file.close()
